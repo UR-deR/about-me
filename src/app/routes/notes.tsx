a8a07746613c37ba6acacc19a11c80337edc60a5
UR-deR <shota.furuno@claves.co.jp>
2024-09-23 00:05:08 +0900

    Add routes for displaying issues

---
+++ src/app/routes/notes.tsx
@@ -0,0 +1,22 @@
+import { Link, useLoaderData } from '@remix-run/react';
+import { GithubIssueClient } from '../utils/GithubIssueClient';
+
+export async function clientLoader() {
+  const issues = await GithubIssueClient.listIssues();
+  return { issues };
+}
+
+export default function Notes() {
+  const { issues } = useLoaderData<typeof clientLoader>();
+  if (!issues) return <div>loading...</div>;
+
+  return (
+    <div>
+      {issues.map((issue) => (
+        <div key={issue.id}>
+          <Link to={`/techs/${issue.number}`}>{issue.title}</Link>
+        </div>
+      ))}
+    </div>
+  );
+}
